422339540159c3137c406daf00190f554027f153
Hrit Amatya <privhritamatya@gmail.com>
2026-09-02 19:48:32 +0545

    Implemented settings theme and discovery feature as well as implement nearby devices scan feature

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,14 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+// Import the plugins
+use tauri_plugin_dialog;
+use tauri_plugin_store;
+
 fn main() {
-    pulse_lib::run()
+    tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())   
+        .plugin(tauri_plugin_store::Builder::default().build())    
+
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
